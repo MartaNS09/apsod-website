06a635f6c8b36286b67915a4ac3c20151408f1b0
MartaNS09 <martacrystal9@gmail.com>
2025-09-25 20:55:56 +0300

    Update meta tags for APSOD

--- src/data/config.ts
+++ src/data/config.ts
@@ -7,12 +7,12 @@ import {
 
 // ==================[ App Info ]================== //
 export const app_info = {
-  title: "Personal Portfolio / cv",
+  title: "APSOD - Digital маркетинг и SEO продвижение",
 
-  description:
-    "Experienced web developer specializing in front-end and full-stack development using JavaScript, Node.js, React, Next.js, and Tailwind CSS. I create responsive, SEO-friendly websites and applications tailored to your business needs. Contact me for high-quality web development services.",
-} as const;
-//
+  description: "Профессиональное продвижение сайтов, 
+SEO-оптимизация, контекстная реклама. Увеличиваем трафик и 
+конверсии для вашего бизнеса.",
+} as const; //
 
 // ==================[ theme configuration ]================== //
 
